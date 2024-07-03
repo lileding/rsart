1b0fd73977797ba8c2a93911666ab0378dc8c245
LI Leding <lileding@gmail.com>
2024-07-04 00:30:18 +0800

    better code

--- src/art/mod.rs
+++ src/art/mod.rs
@@ -25,22 +25,21 @@ impl Wake for EventWaker {
     }
 }
 
+type Task = Pin<Box<dyn Future<Output = ()> + Send>>;
+
 pub struct Runtime {
     next_token: Cell<usize>,
     poll: RefCell<mio::Poll>,
-    event_handlers: RefCell<HashMap<mio::Token, Waker>>,
+    pending_tasks: RefCell<HashMap<mio::Token, Waker>>,
     active_tasks: RefCell<VecDeque<Task>>,
 }
-
-unsafe impl Send for Runtime {}
-unsafe impl Sync for Runtime {}
 
 impl Runtime {
     pub fn new() -> Result<Arc<Self>> {
         Ok(Arc::new(Runtime {
             next_token: Cell::new(1000),
             poll: RefCell::new(mio::Poll::new()?),
-            event_handlers: RefCell::new(HashMap::new()),
+            pending_tasks: RefCell::new(HashMap::new()),
             active_tasks: RefCell::new(VecDeque::new()),
         }))
     }
@@ -49,15 +48,13 @@ impl Runtime {
         CONTEXT.with_borrow(Arc::clone)
     }
 
-    pub fn block_on<F>(&self, task: F) -> F::Output where
-            F: Future<Output = ()> + Send + Sync + 'static {
+    fn block_on<F>(&self, task: F) -> F::Output where
+        F: Future<Output = ()> + Send + 'static {
         let task = Box::pin(task);
         self.active_tasks.borrow_mut().push_back(task);
-        loop {
-            let mut tq = self.active_tasks.borrow_mut();
-            let task = tq.pop_front();
-            drop(tq);
-            if let Some(mut task) = task {
+        while !self.active_tasks.borrow().is_empty() || !self.pending_tasks.borrow().is_empty() {
+            let mut tq = self.active_tasks.replace(VecDeque::new());
+            while let Some(mut task) = tq.pop_front() {
                 let event_waker = Arc::new(EventWaker { task: Mutex::new(None) });
                 let waker = Arc::clone(&event_waker).into();
                 let mut cx = Context::from_waker(&waker);
@@ -65,20 +62,22 @@ impl Runtime {
                     event_waker.task.lock().unwrap().replace(task);
                 }
             }
-            if !self.event_handlers.borrow().is_empty() {
-                self.wait().unwrap();
-            }
-            if self.active_tasks.borrow().is_empty() {
-                break;
-            }
+            self.wait().unwrap();
         }
     }
 
+    fn spawn<F: Future<Output = ()> + Send + 'static>(&self, task: F) -> F::Output {
+        self.active_tasks.borrow_mut().push_back(Box::pin(task));
+    }
+
     fn wait(&self) -> Result<()> {
+        if self.pending_tasks.borrow().is_empty() {
+            return Ok(());
+        }
         let mut events = mio::Events::with_capacity(10);
         self.poll.borrow_mut().poll(&mut events, None)?;
         for event in &events {
-            if let Some(waker) = self.event_handlers.borrow_mut().remove(&event.token()) {
+            if let Some(waker) = self.pending_tasks.borrow_mut().remove(&event.token()) {
                 waker.wake_by_ref();
             }
         }
@@ -91,13 +90,13 @@ impl Runtime {
 
     fn submit(&self, event: &mut impl mio::event::Source, interest: mio::Interest, waker: Waker) -> Result<()> {
         let token = self.next_token.get();
-        self.event_handlers.borrow_mut().insert(mio::Token(token), waker);
+        self.pending_tasks.borrow_mut().insert(mio::Token(token), waker);
         self.poll.borrow_mut().registry().register(event, mio::Token(token), interest)?;
         self.next_token.set(token + 1);
         Ok(())
     }
 
-    pub fn sleep(&self, duration: Duration) -> Result<Timer> {
+    fn sleep(&self, duration: Duration) -> Result<Timer> {
         let mut timer = timer::TimerFd::new(timer::ClockId::Monotonic)?;
         timer.set_timeout(&duration)?;
         let state = Arc::new(Mutex::new(TimerState {
@@ -108,17 +107,14 @@ impl Runtime {
         self.submit(&mut timer, mio::Interest::READABLE, Waker::from(waker))?;
         Ok(Timer { state, timer })
     }
-
 }
-
-pub type Task = Pin<Box<dyn Future<Output = ()> + Send + Sync>>;
 
 thread_local! {
     pub static CONTEXT: RefCell<Arc<Runtime>> = RefCell::new(Runtime::new().unwrap());
 }
 
-pub fn spawn<F: Future<Output = ()> + Send + Sync + 'static>(task: F) -> F::Output {
-    Runtime::current().active_tasks.borrow_mut().push_back(Box::pin(task));
+pub fn spawn<F: Future<Output = ()> + Send + 'static>(task: F) -> F::Output {
+    Runtime::current().spawn(task);
 }
 
 pub struct Timer {
@@ -159,10 +155,10 @@ impl Wake for TimerWaker {
 }
 
 pub fn block_on<F>(task: F) where
-    F: Future<Output = ()> + Send + Sync + 'static {
+    F: Future<Output = ()> + Send + 'static {
     Runtime::current().block_on(task)
 }
 
-pub fn sleep(duration: Duration) -> Result<Timer> {
-    Runtime::current().sleep(duration)
+pub fn sleep(duration: Duration) -> Timer {
+    Runtime::current().sleep(duration).unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ mod art;
 
 fn main() {
     art::CONTEXT.set(art::Runtime::new().unwrap());
-    art::block_on(async {
-        println!("task 1");
-        art::spawn(async { println!("task 2"); });
-        art::sleep(std::time::Duration::from_secs(1)).unwrap().await;
-        art::spawn(async {
-            art::sleep(std::time::Duration::from_secs(3)).unwrap().await;
-            println!("task 3");
-        });
-        println!("task 1 complete");
+    art::block_on(async_main());
+}
+
+async fn async_main() {
+    println!("task 1");
+    art::spawn(async { println!("task 2"); });
+    art::sleep(std::time::Duration::from_secs(1)).await;
+    art::spawn(async {
+        art::sleep(std::time::Duration::from_secs(3)).await;
+        println!("task 3");
     });
+    println!("task 1 complete");
 }
 
